9ead361234779ec21f84cf3a272c2a81407dae9c
YusufKARA61 <yusufkara061@gmail.com>
2023-06-02 12:28:02 +0300

    0206Ofis
    Müşteri Yorumları Düzenlendi

--- static/front/js/custom.js
+++ static/front/js/custom.js
@@ -1,23 +1,15 @@
-$(document).ready(function() {
-    $("#yapi_sinifi").change(function() {
-      var yapiSinifi = $(this).val();
+document.addEventListener("DOMContentLoaded", function () {
+  var carousel = document.getElementById("carouselBasicExample");
+  var carouselInstance = new bootstrap.Carousel(carousel);
 
-      $.ajax({
-        type: "POST",
-        url: "/projeler",
-        data: JSON.stringify({ yapi_sinifi: yapiSinifi }),
-        contentType: "application/json",
-        success: function(response) {
-          var options = "";
-          $.each(response, function(index, proje) {
-            options += "<option value='" + proje + "'>" + proje + "</option>";
-          });
-          $("#projeler").html(options);
-        },
-        error: function() {
-          console.log("Bir hata oluştu. Projeler getirilemedi!");
-        }
-      });
-    });
+  var prevButton = document.querySelector('[data-mdb-slide="prev"]');
+  var nextButton = document.querySelector('[data-mdb-slide="next"]');
+
+  prevButton.addEventListener("click", function () {
+    carouselInstance.prev();
   });
 
+  nextButton.addEventListener("click", function () {
+    carouselInstance.next();
+  });
+});
